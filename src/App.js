1975389745392b4654b14f1e39a86dbc129dfc38
mahmoodfoysal <foysalcse033@gmail.com>
2021-10-20 21:10:42 +0600

    Completd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { createContext, useState } from 'react';
 import './App.css';
+import
+{
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Service from './components/Service/Service';
+import ServiceDetail from './components/ServiceDetail/ServiceDetail';
+import AboutUs from './components/AboutUs/AboutUs';
+import FAQ from './components/FAQ/FAQ';
+import Footer from './components/Footer/Footer';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
-function App() {
+export const UserContext = createContext();
+
+function App()
+{
+  const [loggedInUser, setLoggedInUser] = useState({});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/services">
+            <Service />
+          </Route>
+          <PrivateRoute exact path="/service/:id">
+            <ServiceDetail />
+          </PrivateRoute>
+          <Route exact path="/about-us">
+            <AboutUs />
+          </Route>
+          <Route exact path="/faq">
+            <FAQ />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBKzMUvCcacnsMx8-55NfNVMDdPcTdbnk",
+    authDomain: "medi-care-48642.firebaseapp.com",
+    projectId: "medi-care-48642",
+    storageBucket: "medi-care-48642.appspot.com",
+    messagingSenderId: "341733180623",
+    appId: "1:341733180623:web:a39c9187e8dc79050e63ba"
+  };
+
+export default firebaseConfig
